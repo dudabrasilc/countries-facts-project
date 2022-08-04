bc862719e78810b5a1835e0430b6ecace5e13dc2
Maria Coitinho <brasildu@gmail.com>
2022-08-04 17:25:24 -0500

    fetch request to public API countries server(with interpolation of input form value). receives data and parses from json into javascript obj. checks if form input is found on data, so then it can return the country and if not, calls function notListed() to display message on page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // Add event listener to the form
     function form() {
     const form = document.getElementById("entry")
-    // console.log(form)
-    form.addEventListener("submit", e => {
+        // console.log(form)
+        form.addEventListener("submit", e => {
         // Prevent reload page default on submit event
         e.preventDefault();
         // console.log(e.target)
@@ -20,9 +20,41 @@ document.addEventListener("DOMContentLoaded", () => {
         // Clear formInput value
         form.reset()
         // console.log(form)
-    })
-    }
 
-})
-form()
-//
+                // FETCH REQUEST
+                return fetch(`https://restcountries.com/v3.1/name/${formInputValue}`)
+                .then(response => response.json())
+                .then(countries => {
+                    // console.log(countries)
+                    const isListed = countries.find(country => {
+                        return formInputValue === country.name.common })
+                        // console.log(isListed)
+                    if (isListed === undefined) {
+                            notListed();
+                    } else {
+                        countries.forEach(country => {
+                            const flag = document.querySelector("#flag");
+                            flag.style.fontSize = "100px";
+                            flag.setAttribute("src", country.flags.png)
+                            const countryName = document.querySelector("#name");
+                            countryName.textContent = isListed.name.common;
+                        })
+                    }
+                                     
+        })
+
+    })
+}}
+)
+
+
+function notListed() {
+    const searchOutput = document.querySelector("#searchOutput")
+    const flag = document.querySelector("#flag")
+    flag.setAttribute("src", "")
+    const countryName = document.querySelector("#name")
+    countryName.innerHTML = "Not listed 😢"
+    countryName.style.fontSize = "60px"
+    
+}
+
