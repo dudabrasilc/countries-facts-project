a02425736a39108526193632284a89633f46f73d
Maria Coitinho <brasildu@gmail.com>
2022-08-04 16:13:23 -0500

    index.html: header tag containing h4, h1, p and form tags. Form tag children: label, input and input tag for button.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
     function form() {
     const form = document.getElementById("entry")
     // console.log(form)
+    form.addEventListener("submit", e => {
+        // Prevent reload page default on submit event
+        e.preventDefault();
+        // console.log(e.target)
+
+        // Get formInput value
+        let formInputValue = document.querySelector("#formInput").value
+        // console.log(formInputValue)
+        
+        // const searchOutput = document.querySelector("#searchOutput")
+        // searchOutput.replaceChildren()
+
+        // Clear formInput value
+        form.reset()
+        // console.log(form)
+    })
     }
 
 })
