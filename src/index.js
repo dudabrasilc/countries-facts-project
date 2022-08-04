4f3026356ab06ac55fc55a868ce09237a8d2ad43
Maria Coitinho <brasildu@gmail.com>
2022-08-04 14:57:08 -0500

    index.html: added essential html tags, added metadata to the head and linked css styles sheet file. index.js: added base URL(rest countries public API), event listener to the DOM. styles.css: added styles to html, body and header tags.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://restcountries.com/v3.1/all"
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Add event listener to the form
+    function form() {
+    const form = document.getElementById("entry")
+    // console.log(form)
+    }
+
+})
+form()
