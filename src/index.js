e1ba65aef8d68b67ff559926b0e39ef5d1e1b921
Maria Coitinho <brasildu@gmail.com>
2022-08-06 10:25:54 -0500

    optmimization of index.js code, debugg on 404 errors, creation of 3 functions for clean and dry code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const BASE_URL = "https://restcountries.com/v3.1/all"
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Add event listener to the form
-    function form() {
+
+
+
+
+function form() {
     const form = document.getElementById("entry")
         // console.log(form)
         form.addEventListener("submit", e => {
@@ -10,51 +13,57 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         // console.log(e.target)
 
-        // Get formInput value
-        let formInputValue = document.querySelector("#formInput").value
-        // console.log(formInputValue)
-        
-        // const searchOutput = document.querySelector("#searchOutput")
-        // searchOutput.replaceChildren()
+         // Get formInput value
+         let countryInput = document.querySelector("#formInput").value
+         // console.log(formInputValue)
 
         // Clear formInput value
         form.reset()
-        // console.log(form)
-
-                // FETCH REQUEST
-                return fetch(`https://restcountries.com/v3.1/name/${formInputValue}`)
-                .then(response => response.json())
-                .then(countries => {
-                    // console.log(countries)
-                    const isListed = countries.find(country => {
-                        return formInputValue === country.name.common })
-                        // console.log(isListed)
-                    if (isListed === undefined) {
-                            notListed();
-                    } else {
-                        countries.forEach(country => {
-                            const flag = document.querySelector("#flag");
-                            flag.style.fontSize = "100px";
-                            flag.setAttribute("src", country.flags.png)
-                            const countryName = document.querySelector("#name");
-                            countryName.textContent = isListed.name.common;
-                        })
-                    }
-                                     
-        })
+        
+        renderCountry(countryInput)
 
     })
-}}
-)
+}
+form()
 
 
 function notListed() {
-    const searchOutput = document.querySelector("#searchOutput")
     const flag = document.querySelector("#flag")
     flag.setAttribute("src", "")
     const countryName = document.querySelector("#name")
     countryName.innerHTML = "Not listed 😢"
     countryName.style.fontSize = "60px"
-    
 }
+
+
+
+function renderCountry(countryInput) {
+    // FETCH REQUEST
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(countries => {
+        const isListed = countries.find(country => {
+            return countryInput === country.name.common })
+            console.log(isListed)
+        if (isListed === undefined) {
+                notListed();
+        } else {
+                const flag = document.querySelector("#flag");
+                flag.setAttribute("src", isListed.flags.png)
+                const countryName = document.querySelector("#name");
+                countryName.textContent = isListed.name.common;
+        }
+                         
+})
+
+}
+
+
+
+
+
+
+})
+
+
 
